4aae7cabcc1b2a16665f4c04cf65696a5b9b0a7d
lahm <l292538769@gmail.com>
2020-02-02 12:22:01 +0800

    arrangeCoins

---
+++ src/ArrangeCoins.kt
@@ -0,0 +1,35 @@
+import kotlin.math.sqrt
+
+/**
+ * 给定一个数字 n，找出可形成完整阶梯行的总行数。
+ * n 是一个非负整数，并且在32位有符号整型的范围内。
+ *
+ * https://leetcode-cn.com/problems/arranging-coins/
+ */
+fun main() {
+    print("result---" + ArrangeCoins.arrangeCoinsByMath(5050))
+    print("result---" + ArrangeCoins.arrangeCoinsByIteration(5050))
+}
+
+class ArrangeCoins {
+    companion object {
+        @JvmStatic
+        fun arrangeCoinsByMath(n: Int): Int {
+            if (n < 0) return 0
+            if (n == 1) return 1
+            return ((-1 + sqrt(1 + 8 * n.toDouble())) / 2).toInt()
+        }
+
+        @JvmStatic
+        fun arrangeCoinsByIteration(n: Int): Int {
+            if (n < 0) return 0
+            var k = 1
+            var num = n
+            while (num - k > k) {
+                num -= k
+                k++
+            }
+            return k
+        }
+    }
+}
