990d7840fd3bfc1a40d875b168d913aab7210d00
long lahm <l292538769@gmail.com>
2020-05-26 20:05:39 +0800

    两数和双指针法

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 
 /**
@@ -12,6 +14,7 @@ public class TwoSum {
         int[] nums = new int[]{2, 7, 11, 15};
         int target = 9;
         System.out.println(twoSum(nums, target)[0] + "--" + twoSum(nums, target)[1]);
+        System.out.println(twoSumP(nums, target)[0] + "--" + twoSumP(nums, target)[1]);
     }
 
     /**
@@ -33,6 +36,28 @@ public class TwoSum {
                 return new int[]{(int) map.get(temp), i};
             } else
                 map.put(nums[i], i);
+        }
+        return new int[]{-1, -1};
+    }
+
+    /**
+     * 双指针两头夹
+     */
+    public static int[] twoSumP(int[] nums, int target) {
+        //先排序
+        Arrays.sort(nums);
+        int head = 0;
+        int tail = nums.length - 1;
+        int tmp;
+        while (head < tail) {
+            tmp = nums[head] + nums[tail];
+            if (tmp == target) {
+                return new int[]{head, tail};//只需要输出一对，输出全部就两个指针往里缩
+            } else if (tmp > target) {
+                tail--;
+            } else {
+                head++;
+            }
         }
         return new int[]{-1, -1};
     }
