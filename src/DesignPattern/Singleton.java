3d5fd6a637fce20e022e8ac2959df545bb17e695
lahm <l292538769@gmail.com>
2020-05-31 23:44:44 +0800

    设计模式 单例 注释

--- src/DesignPattern/Singleton.java
+++ src/DesignPattern/Singleton.java
@@ -51,7 +51,7 @@ public class Singleton {
         private static final Singleton INSTANCE = new Singleton();
     }
 
-    public static Singleton getSingleByHolder() {
+    public static Singleton getInstance() {
         return SingletonHolder.INSTANCE;
     }
 }
