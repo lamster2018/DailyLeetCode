6ad5cd3f2827fe54a8b45e5aee802f2bd01756de
long lahm <l292538769@gmail.com>
2020-06-05 09:49:50 +0800

    已知先，中，求后序,重建树

--- src/Shu/BinaryTreeSearch.java
+++ src/Shu/BinaryTreeSearch.java
@@ -2,10 +2,7 @@ package Shu;
 
 import mode.TreeNode;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.*;
 
 public class BinaryTreeSearch {
     public static void main(String[] args) {
@@ -26,6 +23,27 @@ public class BinaryTreeSearch {
         post(root + 1, start, i - 1);  //递归处理左子树
         post(root + 1 + i - start, i + 1, end);  //递归处理右子树
         System.out.print(" " + midArr[i]);//后序的输出是最后
+    }
+
+    // 已知先，中，求后序,重建树
+    // 缓存中序遍历数组每个值对应的索引
+    private static Map<Integer, Integer> indexForInOrders = new HashMap<>();
+
+    public static TreeNode reConstructBinaryTree(int[] pre, int[] in) {
+        for (int i = 0; i < in.length; i++)
+            indexForInOrders.put(in[i], i);
+        return reConstructBinaryTree(pre, 0, pre.length - 1, 0);
+    }
+
+    private static TreeNode reConstructBinaryTree(int[] pre, int preL, int preR, int inL) {
+        if (preL > preR)
+            return null;
+        TreeNode root = new TreeNode(pre[preL]);
+        int inIndex = indexForInOrders.get(root.value);
+        int leftTreeSize = inIndex - inL;
+        root.left = reConstructBinaryTree(pre, preL + 1, preL + leftTreeSize, inL);
+        root.right = reConstructBinaryTree(pre, preL + leftTreeSize + 1, preR, inL + leftTreeSize + 1);
+        return root;
     }
 
     //已知中，后，求先序
--- src/mode/TreeNode.java
+++ src/mode/TreeNode.java
@@ -1,6 +1,14 @@
 package mode;
 
 public class TreeNode {
+    public TreeNode() {
+
+    }
+
+    public TreeNode(int value) {
+        this.value = value;
+    }
+
     public TreeNode left;
     public TreeNode right;
     public int value;
