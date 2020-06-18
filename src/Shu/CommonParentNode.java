f6729797957020974400ecb6bb13e457267a8039
long lahm <l292538769@gmail.com>
2020-06-18 10:36:22 +0800

    最低公共祖先

---
+++ src/Shu/CommonParentNode.java
@@ -0,0 +1,46 @@
+package Shu;
+
+import mode.TreeNode;
+
+public class CommonParentNode {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 在二叉查找树中，两个节点 p, q 的公共祖先 root 满足 root.val >= p.val && root.val <= q.val
+     *
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null)
+            return root;
+        if (root.val > p.val && root.val > q.val)
+            return lowestCommonAncestor(root.left, p, q);
+        if (root.val < p.val && root.val < q.val)
+            return lowestCommonAncestor(root.right, p, q);
+        return root;
+    }
+
+    /**
+     * 普通二叉树
+     * 在左右子树中查找是否存在 p 或者 q，如果 p 和 q 分别在两个子树中，那么就说明根节点就是最低公共祖先。
+     *
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
+
+    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q)
+            return root;
+        TreeNode left = lowestCommonAncestor2(root.left, p, q);
+        TreeNode right = lowestCommonAncestor2(root.right, p, q);
+        return left == null ? right : right == null ? left : root;
+    }
+
+}
